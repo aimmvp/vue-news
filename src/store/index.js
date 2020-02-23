e39f25710dfda98a1b7f614e49116e38616e2508
sungil kang <aimmvp@gmail.com>
2020-02-23 21:04:43 +0900

    스토어 속성 모듈화

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList } from '../api/index.js';
+
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
@@ -10,52 +12,12 @@ export const store = new Vuex.Store({
         ask: [],
         jobs: [],
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_ASK(state, asks) {
-            state.asks = asks;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-    },
+    // mutations: mutations,
+    mutations,
     getters: {
         fetchedAsk(state) {
             return state.ask;
         }
     },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    // console.log(response.data);
-                    context.commit('SET_NEWS', response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-        FETCH_ASK({ commit }) {
-            fetchAskList()
-                .then(({ data }) => {
-                    // console.log(response.data);
-                    commit('SET_ASK', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-        FETCH_JOBS({ commit }) {
-            fetchJobsList()
-                .then(({ data }) => {
-                    // console.log(response.data);
-                    commit('SET_JOBS', data);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
-        },
-    }
+    actions 
 });
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,37 +1,29 @@
 <template>
     <div>
-        <div v-for="ask in asks" v-bind:key="ask"> {{ ask.title }}</div>
+        <div v-for="item in askItems" v-bind:key="item"> {{ item.title }}</div>
     </div>
 </template>
 
 <script>
+import { mapGetters} from 'vuex';
 // import {fetchAskList} from '../api/index.js';
-import { mapState } from 'vuex';
 export default {
-    data() {
-        return {
-            asks: []
-        }
-    },
     computed: {
-        ...mapState({
-            asks: state => state.ask
-        })
+        ...mapGetters({
+            askItems: 'fetchedAsk'
+        }),
+
+        // #2
+        // ...mapState({
+        //     ask: state => state.ask
+        // })
+        // #1
+        // ask() {
+        //     return this.$store.state;
+        // }
     },
     created() {
         this.$store.dispatch('FETCH_ASK');
-        // var vm = this;
-
-        // fetchAskList()
-        //     .then(function(response) {
-        //         vm.asks = response.data;
-        //     })
-        //     .catch(function(error) {
-        //         console.log(error);
-        //     });
-        // fetchAskList()
-        //     .then(response => this.asks = response.data)
-        //     .catch( error => console.log(error));
     },
     
 }
